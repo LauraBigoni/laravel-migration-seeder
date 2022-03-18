1e91cf7d38d0e065874b53d79a55718a66119d93
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-18 18:08:22 +0100

    added seeds in db

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -15,6 +15,16 @@ class TrainsTableSeeder extends Seeder
     {
         $train = new Train();
 
-        $train->
+        $train->company_name = 'Trenitalia';
+        $train->departure_station = 'Firenze';
+        $train->arrival_station = 'Bari';
+        $train->departure_time = '2022-03-18 10:14:00';
+        $train->arrival_time = '2022-03-18 22:30:20';
+        $train->code_number = 'HZ873R';
+        $train->carriages = 9;
+        $train->in_time = 0;
+        $train->canceled = 0;
+
+        $train->save();
     }
 }
