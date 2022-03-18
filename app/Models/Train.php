fe8d3f8a9d2fe53ca88a97e938362ef2fdf94e4a
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-18 18:24:52 +0100

    added fillable

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $fillable = [
+        'company_name',
+        'departure_station',
+        'arrival_station',
+        'departure_time',
+        'arrival_time',
+        'code_number',
+        'carriages',
+        'in_time',
+        'canceled'
+    ];
 }
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,18 +13,69 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        $train = new Train();
+        $trains = [
+            [
+                'company_name' => 'Trenitalia',
+                'departure_station'=> 'Bergamo',
+                'arrival_station'=> 'Napoli',
+                'departure_time'=> '2022-03-18 08:01:17',
+                'arrival_time'=> '2022-03-18 18:01:17',
+                'code_number'=> 'AU554Y',
+                'carriages' => 12,
+                'in_time' => false,
+                'canceled' => false
+            ],
+            [
+                'company_name' => 'Trenitalia',
+                'departure_station'=> 'Napoli',
+                'arrival_station'=> 'Taranto',
+                'departure_time'=> '2022-03-18 08:01:17',
+                'arrival_time'=> '2022-03-18 18:01:17',
+                'code_number'=> 'AT554Y',
+                'carriages' => 3,
+                'in_time' => false,
+                'canceled' => false
+            ],
+            [
+                'company_name' => 'Trenitalia',
+                'departure_station'=> 'Trento',
+                'arrival_station'=> 'Venezia',
+                'departure_time'=> '2022-03-18 08:01:17',
+                'arrival_time'=> '2022-03-18 18:01:17',
+                'code_number'=> 'VU554Y',
+                'carriages' => 9,
+                'in_time' => false,
+                'canceled' => false
+            ],
+            [
+                'company_name' => 'Trenitalia',
+                'departure_station'=> 'Roma',
+                'arrival_station'=> 'Ancona',
+                'departure_time'=> '2022-03-18 08:01:17',
+                'arrival_time'=> '2022-03-18 18:01:17',
+                'code_number'=> 'SU554Y',
+                'carriages' => 11,
+                'in_time' => false,
+                'canceled' => false
+            ],
+            [
+                'company_name' => 'Trenitalia',
+                'departure_station'=> 'Treviso',
+                'arrival_station'=> 'Genova',
+                'departure_time'=> '2022-03-18 08:01:17',
+                'arrival_time'=> '2022-03-18 18:01:17',
+                'code_number'=> 'KU554Y',
+                'carriages' => 2,
+                'in_time' => false,
+                'canceled' => false
+            ],
+        ];
 
-        $train->company_name = 'Trenitalia';
-        $train->departure_station = 'Firenze';
-        $train->arrival_station = 'Bari';
-        $train->departure_time = '2022-03-18 10:14:00';
-        $train->arrival_time = '2022-03-18 22:30:20';
-        $train->code_number = 'HZ873R';
-        $train->carriages = 9;
-        $train->in_time = 0;
-        $train->canceled = 0;
-
-        $train->save();
+        
+        foreach ($trains as $train) {
+            $new_train =  new Train();
+            $new_train->fill($train);
+            $new_train->save();
+        }
     }
 }
